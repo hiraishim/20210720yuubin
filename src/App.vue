6e2a2842cc82d24f359336ce49be73cfd1baca00
hiraishim <hiramaki@gmail.com>
2021-07-22 14:13:19 +0900

    追記

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="app">
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+
+</style>
---
+++ src/views/Form.vue
@@ -0,0 +1,34 @@
+<template>
+<div class ="form">
+  <input type ="text" v-model="zipCode"  placeholder="000-0000"/>
+  <button @click="searchAddress">住所自動入力</button>
+  <input type ="text" v-model="address" />
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  props:["zipcode"],
+  data() {
+    return {
+      zipcode: '',
+      address: '',
+    };
+  },
+  methods: {
+    async created() {
+      const item = await axios.get(
+        https://apis.postcode-jp.com/api/v4/postcodes/string?string
+      )
+      then.((response)=>{
+        this.address =""})
+      })
+    }
+  
+
+</script>
+<style scoped>
+.form{
+  width
+}
